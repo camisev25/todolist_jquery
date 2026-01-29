1d2eb84b47e07b14625d4f38135304002d5c45c2
camisev25 <camilasevalho@gmail.com>
2026-01-29 12:44:18 +0000

    Created the to do list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-li.addClass("strike");
+function newItem() {
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
 
-li.addClass("delete");
+  li.append(inputValue);
 
-li.on("click", function() {
-    li.addClass("strike");
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", crossOut);
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", function () {
+    li.addClass("delete");
   });
+
+}
+
+$('#input').on('keypress', function (e) {
+  if (e.which === 13) {
+    newItem();
+    e.preventDefault();
+  }
+});
+
+  $('#list').sortable();
